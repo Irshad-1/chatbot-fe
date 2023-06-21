12305b900561e8069c89d315e319132b5f76f825
Md Irshad <md.irshad@indusnet.co.in>
2023-06-21 10:26:27 +0530

    project initialization

---
+++ src/Components/common/SiteRoutes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Signup } from "../Signup";
+import { Login } from "../Login";
+import { Details } from "../Details";
+import PageNotFound from "../error-pages/PageNotFound";
+
+function SiteRoutes() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/page-not-found" element={<PageNotFound />} />
+                    <Route path="*" element={<Navigate to="/page-not-found" />} />
+                    <Route path='/' element={<Details />} />
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default SiteRoutes;
